0bd022901a2a61a3beb5c1bef216f53662670fbc
Nicolo John Davis <nicolodavis@gmail.com>
2022-09-28 09:17:16 +0800

    add more notes

--- src/main.rs
+++ src/main.rs
@@ -32,34 +32,34 @@ impl Mode {
 fn translate(key: u8, data: &Data) -> &'static str {
     match data.mode {
         Mode::Sharp => match key {
-            60 => "c",
-            61 => "cis",
-            62 => "d",
-            63 => "dis",
-            64 => "e",
-            65 => "f",
-            66 => "fis",
-            67 => "g",
-            68 => "gis",
-            69 => "a",
-            70 => "ais",
-            71 => "b",
+            48 | 60 | 72 | 84 => "c",
+            49 | 61 | 73 => "cis",
+            50 | 62 | 74 => "d",
+            51 | 63 | 75 => "dis",
+            52 | 64 | 76 => "e",
+            53 | 65 | 77 => "f",
+            54 | 66 | 78 => "fis",
+            55 | 67 | 79 => "g",
+            56 | 68 | 80 => "gis",
+            57 | 69 | 81 => "a",
+            58 | 70 | 82 => "ais",
+            59 | 71 | 83 => "b",
             _ => "",
         },
 
         Mode::Flat => match key {
-            60 => "c",
-            61 => "des",
-            62 => "d",
-            63 => "ees",
-            64 => "e",
-            65 => "f",
-            66 => "ges",
-            67 => "g",
-            68 => "aes",
-            69 => "a",
-            70 => "bes",
-            71 => "b",
+            48 | 60 | 72 | 84 => "c",
+            49 | 61 | 73 => "des",
+            50 | 62 | 74 => "d",
+            51 | 63 | 75 => "ees",
+            52 | 64 | 76 => "e",
+            53 | 65 | 77 => "f",
+            54 | 66 | 78 => "ges",
+            55 | 67 | 79 => "g",
+            56 | 68 | 80 => "aes",
+            57 | 69 | 81 => "a",
+            58 | 70 | 82 => "bes",
+            59 | 71 | 83 => "b",
             _ => "",
         },
     }
@@ -100,9 +100,8 @@ fn on_midi(_timestamp: u64, event: &[u8], data: &mut Data) {
     }
 }
 
-fn get_through_port(client: &MidiInput) -> Result<MidiInputPort> {
-    let midi_through_port =
-        std::env::var("MIDI_THROUGH_PORT").unwrap_or("Midi Through".to_string());
+fn get_input_port(client: &MidiInput) -> Result<MidiInputPort> {
+    let midi_through_port = std::env::var("MIDI_INPUT_PORT").unwrap_or("Keystation".to_string());
     for port in client.ports() {
         let name = client.port_name(&port)?;
         if name.contains(&midi_through_port) {
@@ -114,7 +113,7 @@ fn get_through_port(client: &MidiInput) -> Result<MidiInputPort> {
 
 fn run() -> Result<()> {
     let client = MidiInput::new("lilypond-midi-entry")?;
-    let port = get_through_port(&client)?;
+    let port = get_input_port(&client)?;
     let data = Data::default();
     let _connection = client.connect(&port, "midi-through", on_midi, data)?;
 
